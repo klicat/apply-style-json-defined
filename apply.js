a910b1022e188cad7679cbaf10f9759d7dd43361
infuzz <39411241+infuzz@users.noreply.github.com>
2020-05-04 18:23:17 +0200

    Update apply.js

--- apply.js
+++ apply.js
@@ -1,38 +1,38 @@
 applyStyles(styles) //apply styles
+{
+	if($('#appstylesheet').length === 0) {
+		$('<style id="appstylesheet" type="text/css"></style>').appendTo("head")
+	}
+
+	mySheet=document.getElementById("appstylesheet").sheet
+	var rules = mySheet.cssRules ? mySheet.cssRules : mySheet.rules
+	for(var selector in styles)
 	{
-		if($('#appstylesheet').length === 0) {
-			$('<style id="appstylesheet" type="text/css"></style>').appendTo("head")
+		for(var selectorContentId in styles[selector]){
+			if(selectorContentId.indexOf('olor')!=-1) {
+				$('body').css('--' + selector + '-' + selectorContentId, styles[selector][selectorContentId])
+			}
 		}
 
-		mySheet=document.getElementById("appstylesheet").sheet
-		var rules = mySheet.cssRules ? mySheet.cssRules : mySheet.rules
-		for(var selector in styles)
-		{
-			for(var selectorContentId in styles[selector]){
-				if(selectorContentId.indexOf('olor')!=-1) {
-					$('body').css('--' + selector + '-' + selectorContentId, styles[selector][selectorContentId])
-				}
-			}
+		var mselector="."+selector
 
-			var mselector="."+selector
-			
-			if(selector.indexOf('Hover')!=-1) mselector=mselector.replace('Hover','')+":hover:not(.active)"
-			if(selector.indexOf('Active')!=-1) mselector=mselector.replace('Active','')+".active"
-			var ruleIndex = -1
-			for (i=0; i<rules.length; i++)
+		if(selector.indexOf('Hover')!=-1) mselector=mselector.replace('Hover','')+":hover:not(.active)"
+		if(selector.indexOf('Active')!=-1) mselector=mselector.replace('Active','')+".active"
+		var ruleIndex = -1
+		for (i=0; i<rules.length; i++)
+		{
+			if (rules[i].selectorText == mselector)
 			{
-				if (rules[i].selectorText == mselector)
-				{
-					ruleIndex = i;
-					$.extend(rules[i].style, styles[selector])
-					break;
-				}
+				ruleIndex = i;
+				$.extend(rules[i].style, styles[selector])
+				break;
 			}
-			if(ruleIndex==-1)
-			{
-				cssStr=""
-				ruleIndex=mySheet.insertRule(mselector + '{' + cssStr + '}', rules.length)
-				$.extend(rules[ruleIndex].style, styles[selector])
-			}
+		}
+		if(ruleIndex==-1)
+		{
+			cssStr=""
+			ruleIndex=mySheet.insertRule(mselector + '{' + cssStr + '}', rules.length)
+			$.extend(rules[ruleIndex].style, styles[selector])
 		}
 	}
+}
