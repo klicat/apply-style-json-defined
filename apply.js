ec77904027871b518f6717bbef8aa2a169b65459
infuzz <39411241+infuzz@users.noreply.github.com>
2020-05-22 20:05:03 +0200

    Update apply.js

--- apply.js
+++ apply.js
@@ -1,38 +1,43 @@
-applyStyles(styles) //apply styles
-{
-	if($('#appstylesheet').length === 0) {
-		$('<style id="appstylesheet" type="text/css"></style>').appendTo("head")
-	}
+applyStyles(styles)
+  {
+    if(!document.getElementById("appstylesheet")) {
+      let style=document.createElement('style')
+      style.id='appstylesheet'
+      style.type="text/css"
+      document.head.appendChild(style)
+    }
 
-	mySheet=document.getElementById("appstylesheet").sheet
-	var rules = mySheet.cssRules ? mySheet.cssRules : mySheet.rules
-	for(var selector in styles)
-	{
-		for(var selectorContentId in styles[selector]){
-			if(selectorContentId.indexOf('olor')!=-1) {
-				$('body').css('--' + selector + '-' + selectorContentId, styles[selector][selectorContentId])
-			}
-		}
+    let mySheet=document.getElementById("appstylesheet").sheet
+    let rules = mySheet.cssRules ? mySheet.cssRules : mySheet.rules
+    for(let selector in styles)
+    {
+      for(let selectorContentId in styles[selector]){
+        if(selectorContentId.indexOf('olor')!=-1) {
+          document.querySelector(":root").style.setProperty('--' + selector + '-' + selectorContentId, styles[selector][selectorContentId])
+        }
+      }
 
-		var mselector="."+selector
+      var mselector="."+selector
 
-		if(selector.indexOf('Hover')!=-1) mselector=mselector.replace('Hover','')+":hover:not(.active)"
-		if(selector.indexOf('Active')!=-1) mselector=mselector.replace('Active','')+".active"
-		var ruleIndex = -1
-		for (i=0; i<rules.length; i++)
-		{
-			if (rules[i].selectorText == mselector)
-			{
-				ruleIndex = i;
-				$.extend(rules[i].style, styles[selector])
-				break;
-			}
-		}
-		if(ruleIndex==-1)
-		{
-			cssStr=""
-			ruleIndex=mySheet.insertRule(mselector + '{' + cssStr + '}', rules.length)
-			$.extend(rules[ruleIndex].style, styles[selector])
-		}
-	}
-}
+      if(selector.indexOf('Hover')!=-1) mselector=mselector.replace('Hover','')+":hover:not(.active)"
+      if(selector.indexOf('Active')!=-1) mselector=mselector.replace('Active','')+".active"
+      let ruleIndex = -1
+      for (let i=0; i<rules.length; i++)
+      {
+        if (rules[i].selectorText == mselector)
+        {
+          ruleIndex = i;
+          //$.extend(rules[i].style, styles[selector])
+          assign(rules[i].style, styles[selector])
+          break;
+        }
+      }
+      if(ruleIndex==-1)
+      {
+        let cssStr=""
+        ruleIndex=mySheet.insertRule(mselector + '{' + cssStr + '}', rules.length)
+        //$.extend(rules[ruleIndex].style, styles[selector])
+        assign(rules[ruleIndex].style, styles[selector])
+      }
+    }
+  }
